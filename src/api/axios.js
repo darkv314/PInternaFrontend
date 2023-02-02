65a15b8ffe1c1e65fa506cfc703837a5751daa4d
Vanessa Tejerina <vanessa.tejerina@jala.university>
2023-02-02 13:26:08 -0400

    Prod

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3000/",
+    // baseURL: "http://localhost:3000/",
+    baseURL: "https://backend-z6f8.onrender.com",
 });
